f028e19ee26c16605dacec743de6d31281451b00
ksugg <ksugg@collegeforcreativestudies.edu>
2016-09-28 08:46:53 -0400

    Project One Main Site
    changes made

--- script.js
+++ script.js
@@ -12,44 +12,81 @@ $(document).ready(function() {
     $("#clock").html(timer.hour + ':' + timer.minutes + ':' + timer.seconds + ' ');
   }, 1000);
 
-  setInterval(function () {
-    $(".box01").replaceWith('<div class= "box02">');
-  }, 2000);
-  setInterval(function () {
-    $(".box02").replaceWith('<div class= "box03">');
-  }, 4000);
-  setInterval(function () {
-    $(".box03").replaceWith('<div class= "box04">');
-  }, 6000);
-   setInterval(function () {
-    $(".box04").replaceWith('<div class= "box05">');
-  }, 8000);
-    setInterval(function () {
-    $("body").css("background-color", "darkblue");
-  }, 10000);
-  //   setInterval(function () {
-  //   $(".box05").replaceWith('<div class= "box06">');
-  // }, 12000);
-     setInterval(function () {
-    $(".box06").replaceWith('<div class= "box05">');
-  }, 18000);
-      setInterval(function () {
-    $("body").css("background-color", "lightblue");
-  }, 20000);
-      setInterval(function () {
-    $(".box05").replaceWith('<div class= "box04">');
-  }, 22000);
-      setInterval(function () {
-    $(".box04").replaceWith('<div class= "box03">');
-  }, 24000);
-      setInterval(function () {
-    $(".box03").replaceWith('<div class= "box02">');
-  }, 26000);
-      setInterval(function () {
-    $(".box02").replaceWith('<div class= "box01">');
-  }, 28000);
 
+    var d = new Date();
+    var currentHour = d.getHours();
+    var currentMinute = d.getMinutes(); 
 
-
-
-   });
+    if (currentHour <= 5) 
+     { 
+        $('body').css('background-color', 'black');
+        $('#container').hide();
+      }
+    else if (currentHour == 6)
+    {
+        $('body').css('background-color', 'rgb(0, 0, 179)');
+        $('#container').show();
+      }
+     else if (currentHour == 6 && currentMinute == 30)
+    {
+        $('body').css('background-color', 'rgb(0, 102, 255)');
+        $('#container').animate({
+          height: '2%',
+        }, 5000);
+      }
+    else if (currentHour == 7)
+    {
+        $('body').css('background-color', 'rgb(0, 204, 255)');
+        $('#container').animate({
+          height: '10%',
+        }, 50000);
+      }
+    else if (currentHour ==8)
+    {
+        $('body').css('background-color', 'rgb(0, 204, 255)');
+        $('#container').animate({
+          height: '100%',
+        }, 300000);
+      }
+    else if (currentHour >= 9)
+    {
+        $('body').css('background-color', 'rgb(0, 204, 255)');
+        $('#container').height('100%');
+        $('.box01').css('background', 'rgb(255, 45, 146)');
+        $('.box02').css('background', 'rgb(255, 62, 0)');
+        $('.box03').css('background', 'rgb(0, 66, 246)');
+        $('.box04').css('background', 'rgb(255, 0, 0)');
+        $('.box05').css('background', 'rgb(182, 0, 228)');
+        $('.box06').css('background', 'rgb(222, 252, 2)');
+        $('.box07').css('background', 'rgb(252, 2, 93)');
+        $('.box08').css('background', 'rgb(255, 45, 146)');
+        $('.box09').css('background', 'rgb(255, 224, 0)');
+        $('.box10').css('background', 'rgb(0, 222, 57)');
+      }
+      else if (currentHour == 12)
+    {
+        $('body').css('background-color', 'rgb(128, 229, 255)');
+      }
+    else if (currentHour == 19)
+    {
+        $('body').css('background-color', 'rgb(0, 204, 255)');
+        $('#container').animate({
+          height: '10%',
+        }, 300000);
+      }
+    else if (currentHour == 20)
+    {
+        $('body').css('background-color', 'rgb(0, 204, 255)');
+        $('#container').animate({
+          height: '2%',
+        }, 50000);
+      }
+    else if (currentHour == 21)
+    {
+        $('body').css('background-color', 'rgb(0, 102, 255)');
+        $('#container').hide()
+      }
+    else {
+        $('body').css('background-color', 'black'); 
+      }
+});
